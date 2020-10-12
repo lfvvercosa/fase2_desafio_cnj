5d17b9fafc637037302392078758e61160089b59
Fernando Oliveira <f.oliveira.aj0@gmail.com>
2020-10-12 20:04:01 -0300

    fonts and header

---
+++ frontend/js/components.js
@@ -0,0 +1,21 @@
+function groupInfoWidth(word) {
+  var width = 1.2 * word.length
+  console.log('width for '+word + ' = '+width+", len = "+word.length)
+  return width
+}
+
+function setGroupInfoWidth(){
+  var i, $infos = $('.rounded-info');
+  for (i=0; i<$infos.length; i++)    {
+    var word =  $infos.eq(i).html()
+    var index = word.indexOf('>') + 1
+    word = word.substr(index)
+    index = word.indexOf('<')
+    word = word.substr(0, index)
+    console.log(word)
+    var width = groupInfoWidth(word);
+    $infos.eq(i).css("width", width + "rem")
+  }
+}
+
+setGroupInfoWidth()
