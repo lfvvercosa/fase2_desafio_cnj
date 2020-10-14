fec766218f1a4e64d01a3d2339698d5798b82337
Gabriel Bandeira <gmbandeira3@gmail.com>
2020-10-14 19:29:24 -0300

    Add endpoints and its main functions

--- backend/django-project/performance/urls.py
+++ backend/django-project/performance/urls.py
@@ -3,6 +3,22 @@ from . import views
 
 
 urlpatterns = [
-    # path('api/view_vara', view_vara)
-    path('vara/<int:vara_id>/', views.view_vara),
+    # Home
+    path('', views.home),
+
+    # Varas
+    path('api/v1/varas/', views.lista_varas),
+    path('api/v1/varas/<int:vara_id>/', views.detalhes_vara),
+    path('api/v1/varas/melhoresVarasNaEtapa/', views.melhores_varas_na_etapa),
+
+    # Etapas
+    path('api/v1/etapas/melhoresEtapas/', views.melhores_etapas),
+    path('api/v1/etapas/pioresEtapas/', views.piores_etapas),
+
+    # Processos
+    path('api/v1/processos/melhoresVaras/', views.melhores_varas),
+
+    # Comentarios
+    path('api/v1/comentarios/', views.lista_comentarios),
+    path('api/v1/comentarios/<int:identificador>/', views.comentario),
 ]
--- backend/django-project/performance/views.py
+++ backend/django-project/performance/views.py
@@ -11,12 +11,94 @@ from rest_framework.status import HTTP_400_BAD_REQUEST, \
 from rest_framework.permissions import AllowAny
 
 
+# Home
 @csrf_exempt
 @api_view(["GET"])
 @permission_classes((AllowAny,))
-def view_vara(request, vara_id):
+def home(request):
+    return Response('home', HTTP_200_OK)
+
+
+# Varas
+@csrf_exempt
+@api_view(["GET"])
+@permission_classes((AllowAny,))
+def lista_varas(request):
+    return Response('lista de varas', HTTP_200_OK)
+
+@csrf_exempt
+@api_view(["GET"])
+@permission_classes((AllowAny,))
+def detalhes_vara(request, vara_id):
+    return Response(f'Detalhes da vara {vara_id}', HTTP_200_OK)
     vara = Vara.objects.get(id=vara_id)
 
     res = VaraSerializer(vara).data
 
     return Response(res, HTTP_200_OK)
+
+@csrf_exempt
+@api_view(["GET"])
+@permission_classes((AllowAny,))
+def melhores_varas_na_etapa(request):
+    identificador = request.GET.get('identificador', None)
+    numeroDeVaras = request.GET.get('numeroDeVaras', None)
+    res = {
+        'identificador': identificador,
+        'numeroDeVaras': numeroDeVaras
+    }
+    return Response(res, HTTP_200_OK)
+
+
+# Etapas
+@csrf_exempt
+@api_view(["GET"])
+@permission_classes((AllowAny,))
+def melhores_etapas(request):
+    identificador = request.GET.get('identificador', None)
+    numeroDeEtapas = request.GET.get('numeroDeEtapas', None)
+    res = {
+        'identificador': identificador,
+        'numeroDeEtapas': numeroDeEtapas
+    }
+    return Response(res, HTTP_200_OK)
+
+@csrf_exempt
+@api_view(["GET"])
+@permission_classes((AllowAny,))
+def piores_etapas(request):
+    identificador = request.GET.get('identificador', None)
+    numeroDeEtapas = request.GET.get('numeroDeEtapas', None)
+    res = {
+        'identificador': identificador,
+        'numeroDeEtapas': numeroDeEtapas
+    }
+    return Response(res, HTTP_200_OK)
+
+
+# Processos
+@csrf_exempt
+@api_view(["GET"])
+@permission_classes((AllowAny,))
+def melhores_varas(request):
+    identificador = request.GET.get('identificador', None)
+    numeroDeVaras = request.GET.get('numeroDeVaras', None)
+    res = {
+        'identificador': identificador,
+        'numeroDeVaras': numeroDeVaras
+    }
+    return Response(res, HTTP_200_OK)
+
+
+# Comentarios
+@csrf_exempt
+@api_view(["GET"])
+@permission_classes((AllowAny,))
+def lista_comentarios(request):
+    return Response('Lista comentarios', HTTP_200_OK)
+
+@csrf_exempt
+@api_view(["GET"])
+@permission_classes((AllowAny,))
+def comentario(request, identificador):
+    return Response(f'Detalhes do comentario {identificador}', HTTP_200_OK)
