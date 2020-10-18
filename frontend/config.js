e496ccc34fa1cb85739ebce2eeb643768d9eeea2
Fernando Oliveira <f.oliveira.aj0@gmail.com>
2020-10-18 16:29:49 -0300

    putting frontend back

---
+++ frontend/config.js
@@ -0,0 +1,3 @@
+Credentials = {
+  "google-maps": "AIzaSyDHOg5WBe8Qi1Fg5919oLFPLRtay2OJf4U" 
+}
---
+++ frontend/js/comparing-courts.js
@@ -0,0 +1,55 @@
+setGraphsComponent()
+setCommentsComponent()
+setTableComponent()
+
+function setGraphsComponent() {
+
+}
+
+function setCommentsComponent() {
+  var comments = 'winwifiwoe wefinwof fwwienfwinfio fwwienfiwenfwnfw wfeionweoifnwiofw efweiunfiwenf wfiuwenfinwe fwiofnweifne'
+  $('#comments').html(comments) 
+}
+
+function setTableComponent() {
+  //best movements table
+  $('#table-best-tab1').html('Mandado-Recebimento')
+  //table-best-tab2
+  //table-best-tab3
+
+  $('#table-best-row-1-name').html('wefnweonfewnfo')
+  $('#table-best-row-1-duration').html('3 meses')
+  $('#table-best-row-1-comments').html('wenfwfoni fwfnwenfiowen woifnwoif')
+  
+  //table-best-row-2-name
+  //table-best-row-2-duration
+  //table-best-row-2-comments
+  
+  //table-best-row-3-name
+  //table-best-row-3-duration
+  //table-best-row-3-comments
+  
+  //table-best-row-4-name
+  //table-best-row-4-duration
+  //table-best-row-4-comments
+  
+  //table-best-row-5-name
+  //table-best-row-5-duration
+  //table-best-row-5-comments
+  
+  //table-best-row-6-name
+  //table-best-row-6-duration
+  //table-best-row-6-comments
+  
+  //table-best-row-7-name
+  //table-best-row-7-duration
+  //table-best-row-7-comments
+  
+  //table-best-row-8-name
+  //table-best-row-8-duration
+  //table-best-row-8-comments
+
+  //table-best-row-9-name
+  //table-best-row-9-duration
+  //table-best-row-9-comments
+}
