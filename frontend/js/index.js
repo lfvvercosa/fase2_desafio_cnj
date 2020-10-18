5dcd6ba4f638f7d5031207c797e04dc62beea7fa
Fernando Oliveira <f.oliveira.aj0@gmail.com>
2020-10-18 06:49:58 -0300

    index.js

---
+++ frontend/js/index.js
@@ -0,0 +1,59 @@
+setHeaderComponent()
+setRankComponent()
+setStatisticsComponent
+setChartComponent
+setFooterComponent
+
+function setHeaderComponent() {
+  //position
+  $('#position-number').html('18ª')
+  
+  //court-name
+  $('#court-name').html('VARA ÚNICA DE ANAJAS')
+  
+  //user
+  $('#username').text('ANTONIO')
+  $('#user-position').text('juiz/Desembargador')
+
+  //group
+  $('#group-amount').text('GRUPO (120 órgãos)')
+  $('#group-el-1').text('Justiça Comercial')
+  $('#group-el-2').text('1° Grau')
+  $('#group-el-3').text('Competência Mista')
+  $('#group-el-4').text('Justiça Comercial')
+}
+
+function setRankComponent() {
+  //bottleneck component
+  //bottoleneck table
+  //best movements component
+  //best movements table
+}
+
+function setStatisticsComponent(){
+  //worst
+  
+  //best
+
+  //duration
+
+  //movements
+
+  //finished
+}
+
+function setChartComponent() {
+
+}
+
+function setFooterComponent() {
+  setFooterRankComponent()
+  setFooterMapComponent()
+}
+
+function setFooterRankComponent() {
+}
+
+function setFooterMapComponent() {
+
+}
