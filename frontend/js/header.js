83bcc74cfd5670e3f8f4eae06953b6a6853c2c31
Fernando Oliveira <f.oliveira.aj0@gmail.com>
2020-10-18 09:45:29 -0300

    controllers

---
+++ frontend/js/header.js
@@ -0,0 +1,20 @@
+setHeaderComponent()
+
+function setHeaderComponent() {
+  //position
+  $('#position-number').html('18ª')
+  
+  //court-name
+  $('#court-name').html('VARA ÚNICA DE ANAJAS')
+  
+  //user
+  $('#username').text('ANTONIO')
+  $('#user-position').text('juiz/Desembargador')
+
+  //group
+  $('#group-amount').text('GRUPO (120 órgãos)')
+  $('#group-el-1').text('Justiça Comercial')
+  $('#group-el-2').text('1° Grau')
+  $('#group-el-3').text('Competência Mista')
+  $('#group-el-4').text('Justiça Comercial')
+}
