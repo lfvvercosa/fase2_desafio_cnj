27da173f830368577b6608599b9e54546c964218
Luiz Felipe Vercosa <lfv.vercosa@gmail.com>
2020-11-24 17:58:19 -0300

    fix: show 'vara' name when clicking on google maps mark

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -1,5 +1,6 @@
 var infowindow = undefined
 var map = undefined
+var markers = []
 
 function addMapToDocument(){
   var googleMapBaseUrl = "https://maps.googleapis.com/maps/api/js"
@@ -8,6 +9,7 @@ function addMapToDocument(){
     +"&libraries="
     +"&v=weekly"
   var googleMap = document.createElement('script');
+  googleMap.setAttribute("async", "")
   googleMap.src = googleMapBaseUrl+googleMapUrlKeyParams
   googleMap.async = true
   document.getElementsByTagName('head')[0].appendChild(googleMap)
@@ -16,7 +18,7 @@ function addMapToDocument(){
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
-    center: new google.maps.LatLng(-11.6182046,-50.6699791),
+    center: new google.maps.LatLng(-15.6182046,-50.6699791),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     gestureHandling: 'none',
     zoomControl: false
@@ -24,18 +26,49 @@ function initMap() {
 
   infowindow = new google.maps.InfoWindow();
 
+
+
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
 }
 
 function populateMap(map, infowindow, locations) {
   var marker, i;
 
-  for (i = 0; i < locations.length; i++) {  
+  deleteMarkers()
+
+  for (i = 0; i < locations.length; i++) { 
+     
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(locations[i][1], locations[i][2]),
       map: map
     });
 
+    markers.push(marker)
+
     google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      
       return function() {
         infowindow.setContent(locations[i][0]);
         infowindow.open(map, marker);
